bbdafaee748889b758fac2d00dee46e01e6ba897
Emilio Rivas <miles.fra.2000@gmail.com>
2021-04-04 04:06:12 -0500

    Cambio velocidad animacion y colores

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function delay(n) {
 function pageTransition(){
     var tl = gsap.timeline();
     tl.to(".loading-screen", {
-        duration: 1.2,
+        duration: 1,
         width: "100%",
         left: "0%",
         ease: "Expo.easeInOut"
     });
 
     tl.to(".loading-screen", {
-        duration: 1,
+        duration: 0.8,
         width: "100%",
         left: "100%",
         ease: "Expo.easeInOut",
