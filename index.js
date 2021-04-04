9c2e8cb2b6678370729d3a2cfe68b0bdc872a8ad
Emilio Rivas <miles.fra.2000@gmail.com>
2021-04-04 03:51:39 -0500

    Animaciones

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-var text = document.getElementById('text');
-var shadow = '';
-for (let index = 0; index < 19; index++) {
-    
-    shadow +=(shadow? ',':'')+index*1+'px '+ index*1+'px 0 #838383';
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
 }
-text.style.textShadow = shadow;
+
+function pageTransition(){
+    var tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 1.2,
+        width: "100%",
+        left: "0%",
+        ease: "Expo.easeInOut"
+    });
+
+    tl.to(".loading-screen", {
+        duration: 1,
+        width: "100%",
+        left: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3
+    });
+
+    tl.set(".loading-screen", { left: "-100%"});
+}
+
+function contentAnimation () {
+
+    var tl = gsap.timeline();
+    tl.from(".animate-this", { duration: 1, y: 30, opacity:0, stagger:0.4, delay:0.2});
+}
+
+$(function() {
+    barba.init({
+
+        sync:true,
+
+        transitions: [
+            {
+                async leave(data){
+                    const done = this.async();
+
+                    pageTransition();
+                    await delay(1000);
+                    done();
+                },
+
+                async enter(data){
+                    contentAnimation();
+                },
+
+                async once(data){
+                    contentAnimation();
+                },
+            },
+        ],
+    });
+});
