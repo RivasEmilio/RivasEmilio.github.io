e34a6fc39bab68271d58e5334430506bef2f6169
Emilio Rivas <miles.fra.2000@gmail.com>
2021-04-09 19:15:06 -0500

    Hot fix responsive

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+
+  $(document).on('click', '.toggle', function(e){
+    $('ul').toggleClass('active1');
+    console.log("clicked");
+  });
+
+
+
+  $(window).on('scroll', function() {
+    if($(window).scrollTop()){
+      $('.nav').addClass('black');
+    }
+    else 
+    {
+      $('.nav').removeClass('black');
+    }
+  });
+
+
+
 function delay(n) {
     n = n || 2000;
     return new Promise((done) => {
@@ -59,65 +79,3 @@ $(function() {
         ],
     });
 });
-
-const options = {
-    root: null,
-    rootMargin: "200px",
-    threshold: 0.9
-  };
-  
-  let revealCallback = (entries, self) => {
-    entries.forEach(entry => {
-      let container = entry.target;
-      let img = entry.target.querySelector("img");
-      const easeInOut = "power3.out";
-      const revealAnim = gsap.timeline({ ease: easeInOut });
-  
-      if (entry.isIntersecting) {
-        revealAnim.set(container, {
-          visibility: "visible"
-        });
-        revealAnim.fromTo(
-          container,
-          {
-            clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-            webkitClipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)"
-          },
-          {
-            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-            webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-            duration: 1,
-            ease: easeInOut
-          }
-        );
-        revealAnim.from(img, 4, {
-          scale: 1.4,
-          ease: easeInOut,
-          delay: -1
-        });
-        self.unobserve(entry.target);
-      }
-    });
-  };
-  
-  let revealObserver = new IntersectionObserver(revealCallback, options);
-  
-  document.querySelectorAll(".reveal").forEach(reveal => {
-    revealObserver.observe(reveal);
-  });
-
-  $(window).on('scroll', function() {
-    if($(window).scrollTop()){
-      $('nav').addClass('black');
-    }
-    else 
-    {
-      $('nav').removeClass('black');
-    }
-  })
-
-  $(document).ready(function(){
-    $('.toggle').click(function(){
-      $('ul').toggleClass('active1');
-    })
-  })
